f8095d3b68c6fde879956eaa11d4501944a96639
tanfending@tobosoft.com.cn <42894946+actior@users.noreply.github.com>
2024-01-31 16:03:09 +0800

    feat: 切换标签页

--- src/components/operation-bar-icon/index.ts
+++ src/components/operation-bar-icon/index.ts
@@ -32,18 +32,24 @@ export function setupOperationBarIcon(element: HTMLDivElement) {
     element.appendChild(icon);
   }
   const iconList = document.querySelectorAll(".operation-bar-icon");
-  for (let i = 0; i < iconList.length; i++) {
+  const operationBarContentBody = document.querySelectorAll(
+    ".operation-bar-content-body"
+  ) as unknown as HTMLDivElement[];
+  const operationBarTitle = document.querySelector(
+    ".operation-bar-content-title"
+  ) as HTMLDivElement;
+  for (let i = 0; i <= iconList.length; ++i) {
     const icon = iconList[i];
     icon.addEventListener("click", () => {
-      for (let j = 0; j < iconList.length; j++) {
+      for (let j = 0; j <= iconList.length; ++j) {
         iconList[j].innerHTML = `<img src="${
           j === i ? centerIconList[j].iconEd : centerIconList[j].icon
         }" class="white icon finger" />`;
-        const operationBarTitle = document.querySelector(
-          ".operation-bar-content-title"
-        ) as HTMLDivElement;
         if (operationBarTitle) {
           operationBarTitle.innerHTML = centerIconList[i].title;
+        }
+        if (operationBarContentBody) {
+          operationBarContentBody[j].style.display = j === i ? "block" : "none";
         }
       }
     });
