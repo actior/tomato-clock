952af6d0391b32ed14114e9717d4375ce95762f5
tanfending@tobosoft.com.cn <42894946+actior@users.noreply.github.com>
2024-02-20 15:42:56 +0800

    feat: 渲染操作按钮

---
+++ src/components/task-operation/index.ts
@@ -0,0 +1,40 @@
+import { CanvasTomato } from "../canvas-tomato/index";
+
+const taskIconList = [
+  {
+    className: "tomato-clock-task-operation-cirle clock-start",
+    title: "START",
+    icon: "/src/static/icons/play_gray.svg",
+  },
+  {
+    className: "tomato-clock-task-operation-cirle clock-pause",
+    title: "PAUSE",
+    icon: "/src/static/icons/pause_gray.svg",
+  },
+  {
+    className: "tomato-clock-task-operation-cirle clock-reset",
+    title: "RESET",
+    icon: "/src/static/icons/reset_gray.svg",
+  },
+];
+export function setupTaskOperation(element: HTMLDivElement) {
+  if (!element) return;
+  for (let i = 0; i < taskIconList.length; i++) {
+    const task = document.createElement("div");
+    task.className = taskIconList[i].className;
+    task.innerHTML = `<img src="${taskIconList[i].icon}" class="tomato-clock-task-operation-cirle-icon" />
+        <div class="tomato-clock-task-operation-cirle-text">${taskIconList[i].title}</div>`;
+    element.appendChild(task);
+  }
+  // 点击开始
+  const clockStartBtn = document.querySelector(
+    ".clock-start"
+  ) as HTMLDivElement;
+  console.log("clockStartBtn: ", clockStartBtn);
+  clockStartBtn.addEventListener("click", () => {
+    CanvasTomato.prototype.run(1500, () => {
+      // circleClock.update(180);
+      console.log("倒计时执行完毕");
+    });
+  });
+}
--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,14 @@ import "./style.css";
 import { setupAddTaskEstimate } from "./components/small-tomato-list/index";
 import { setupOperationBarIcon } from "./components/operation-bar-icon/index";
 import { CanvasTomato } from "./components/canvas-tomato/index";
+import { setupTaskOperation } from "./components/task-operation/index";
 
 const circleClock = new (CanvasTomato as any)('canvas');
-circleClock.run(1500, () => {
-  console.log("倒计时执行完毕");
-});
+circleClock.update(1500);
+// circleClock.run(1500, () => {
+//   console.log("倒计时执行完毕");
+// });
 
+setupTaskOperation(document.querySelector(".tomato-clock-task-operation") as HTMLDivElement);
 setupAddTaskEstimate(document.querySelector(".tomato-rate-list") as HTMLDivElement);
 setupOperationBarIcon(document.querySelector(".operation-bar") as HTMLDivElement);
-
